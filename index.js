52c57a1a5342225fe9b0627b730d49420315c15d
manu <73004915+raccoondotcom@users.noreply.github.com>
2020-10-23 21:41:28 -0300

    add main files

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const HangmanGame = require('./hangman-game');
+const client = new Discord.Client();
+
+const hangman = new HangmanGame(client);
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', msg => {
+	if (msg.content.toLowerCase() === `${prefix}hangman`) {
+		hangman.newGame(msg);
+	}
+	else if (msg.content.includes('gay')) {
+		msg.channel.send('Lol gay');
+	}
+});
+
+// eslint-disable-next-line no-undef
+client.login(token);
